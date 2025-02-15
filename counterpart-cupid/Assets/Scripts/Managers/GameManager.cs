22bd09fccd83d84281432c0af972c68a6ae3ae42
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-02-15 17:51:16 -0500

    Game Over Screen working

--- counterpart-cupid/Assets/Scripts/Managers/GameManager.cs
+++ counterpart-cupid/Assets/Scripts/Managers/GameManager.cs
@@ -5,25 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance = null; 
-
+    public static GameManager Instance = null;
+    public static string lastSceneName = null;
+    private bool isDead = false;
     private int score = 0;
     [SerializeField] private float timer;
 
     void Awake() {
         Instance = this;
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastSceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead) {
+            return;
+        }
         timer -= Time.deltaTime;
         if (timer <= 0) {
             timer = 0;
@@ -42,6 +46,7 @@ public class GameManager : MonoBehaviour
     }
 
     void Lose() {
+        isDead = true;
         SceneManager.LoadSceneAsync("GameOverScreen");
     }
 
