72155cc41dc838e8ccf73589a238e9c4c4b29390
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2025-02-16 10:54:07 -0500

    fix grapple gun prefab

--- counterpart-cupid/Assets/Scripts/GrappleGun.cs
+++ counterpart-cupid/Assets/Scripts/GrappleGun.cs
@@ -35,6 +35,7 @@ public class GrappleGun : MonoBehaviour
     }
 
     void Grab() {
+        print("G");
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
