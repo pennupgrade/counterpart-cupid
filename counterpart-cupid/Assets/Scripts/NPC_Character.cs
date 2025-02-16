c46f69ecf4a10836b02930967091d12084a850a6
Jackie Li <aurorali99@gmail.com>
2025-02-16 11:16:05 -0500

    Uh, the particles spawn?
    I think the gameplay on this branch is a tad buggy.

--- counterpart-cupid/Assets/Scripts/NPC_Character.cs
+++ counterpart-cupid/Assets/Scripts/NPC_Character.cs
@@ -11,6 +11,9 @@ public class NPC_Character : MonoBehaviour
     public int shapeSet;
     public int setElement;
     public Sprite shapeAttribute;
+
+    [SerializeField]
+    private GameObject particles;
     [SerializeField] private GameObject attributeUI;
     private UnityEngine.AI.NavMeshAgent agent;
 
@@ -30,6 +33,9 @@ public class NPC_Character : MonoBehaviour
             this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
             attributeUI.GetComponent<SpriteRenderer>().sprite = shapeAttribute;
         }
+
+        // Ensure that particles is turned off.
+        particles.SetActive(false);
     }
 
     // Update is called once per frame
@@ -75,11 +81,23 @@ public class NPC_Character : MonoBehaviour
     IEnumerator Matched(NPC_Character other)
     {
         // start particles
+        Debug.Log("Matched!");
         this.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        particles.SetActive(true);
+        ParticleSystem ps = particles.GetComponent<ParticleSystem>();
+        if (ps != null)
+        {
+            ps.Play();
+        }
         DisableNavMeshAgent();
         GetComponent<Rigidbody>().isKinematic = true;
         other.GetComponent<Rigidbody>().isKinematic = true;
         yield return new WaitForSeconds(2);
+        if (ps != null)
+        {
+            ps.Stop();
+        }
+        particles.SetActive(false);
         GameManager.AddScore(1);
         Destroy(gameObject);
         Destroy(other.gameObject);
