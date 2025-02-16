3a9e136639f0019c5718a67ac46946513cd069e2
Jackie Li <aurorali99@gmail.com>
2025-02-16 10:03:10 -0500

    Added a particle system effect.
    We can use it if we want to or not. Currently attempting to figure out a way to incorporate.

--- counterpart-cupid/Assets/Scripts/Score.cs
+++ counterpart-cupid/Assets/Scripts/Score.cs
@@ -8,7 +8,7 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<TMP_Text>().text = "Score: " + GameManager.GetScore();
+        GetComponent<TMP_Text>().text = GameManager.GetScore().ToString();
         Destroy(GameManager.Instance.gameObject);
     }
 }
